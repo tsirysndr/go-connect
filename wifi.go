85bae86f100dc5b00ca4e10a68b95aa193b52095
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2020-01-13 08:23:52 +0300

    Add Wifi controls

--- wifi.go
+++ wifi.go
@@ -3,11 +3,14 @@ package connect
 import (
 	"encoding/json"
 	"reflect"
+
+	"github.com/tsirysndr/dbus"
 )
 
 type WifiService service
 
-type Services struct {
+type Service struct {
+	Path                     dbus.ObjectPath
 	Type                     string            `json:"Type,omitempty"`
 	Security                 string            `json:"Security,omitempty"`
 	State                    string            `json:"State,omitempty"`
@@ -60,13 +63,23 @@ type Proxy struct {
 	Method string `json:"Method,omitempty"`
 }
 
-func (s *WifiService) IsAvailable() {
+func (s *WifiService) IsAvailable() bool {
+	info, _ := s.manager.GetTechnologyInfo("wifi")
+	return info.Type != ""
 }
 
-func (s *WifiService) IsEnabled() {
+func (s *WifiService) IsEnabled() bool {
+	info, _ := s.manager.GetTechnologyInfo("wifi")
+	if info.Type == "" {
+		return false
+	}
+	return info.Powered
 }
 
-func (s *WifiService) ToggleState() {
+func (s *WifiService) ToggleState(state bool) error {
+	bo := s.manager.GetTechnologyInterface("wifi")
+	call := bo.Call("net.connman.Technology.SetProperty", 0, "Powered", dbus.MakeVariant(state))
+	return call.Err
 }
 
 func (s *WifiService) Scan() error {
@@ -75,8 +88,8 @@ func (s *WifiService) Scan() error {
 	return bo.Call("net.connman.Technology.Scan", 0).Store(res)
 }
 
-func (s *WifiService) GetNetworks() ([]Services, error) {
-	networks := []Services{}
+func (s *WifiService) GetNetworks() ([]Service, error) {
+	networks := []Service{}
 	bo := s.manager.GetManagerInterface()
 	res := new([]interface{})
 	err := bo.Call("net.connman.Manager.GetServices", 0).Store(res)
@@ -86,8 +99,9 @@ func (s *WifiService) GetNetworks() ([]Services, error) {
 	for _, item := range *res {
 		if reflect.ValueOf(item).Kind() == reflect.Slice {
 			v := reflect.ValueOf(item)
-			data := Services{}
+			data := Service{}
 			Decode(v.Index(1).Interface(), &data)
+			data.Path = v.Index(0).Interface().(dbus.ObjectPath)
 			networks = append(networks, data)
 		}
 	}
@@ -96,15 +110,29 @@ func (s *WifiService) GetNetworks() ([]Services, error) {
 }
 
 func (s *WifiService) Connect() {
+	// TODO
 }
 
-func (s *WifiService) Disconnect() {
+func (s *WifiService) Disconnect(path string) error {
+	bo := s.manager.GetServiceInterface(dbus.ObjectPath(path))
+	call := bo.Call("net.connman.Service.Disconnect", 0)
+	return call.Err
 }
 
-func (s *WifiService) Remove() {
+func (s *WifiService) Remove(path string) error {
+	bo := s.manager.GetServiceInterface(dbus.ObjectPath(path))
+	call := bo.Call("net.connman.Service.Remove", 0)
+	return call.Err
 }
 
-func (s *WifiService) GetConnectedWifi() {
+func (s *WifiService) GetConnectedWifi() *Service {
+	networks, _ := s.GetNetworks()
+	for _, item := range networks {
+		if item.State == "online" {
+			return &item
+		}
+	}
+	return nil
 }
 
 func (s *WifiService) HasNetworkConnection() {
